d9bbf7a99046f592f50267c835a41496c43b9a63
Kshitij Mudgal <iamkshitij@Kshitijs-MacBook-Pro.local>
2021-03-31 01:31:20 +0530

    [first commit] created new prismic blog

---
+++ prismic-configuration.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // The repo name for your Prismic repository (found in the url of your repo: https://my-blog-app.prismic.io/)
+  prismicRepo: 'my-blog-app',
+
+  // Release ID to build (requires access)
+  releaseID: '',
+
+  // access token to access the release
+  accessToken: '',
+}
---
+++ src/components/layouts/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default () => (
+  <footer className="container">
+   <p>
+     Copyright @ 2021
+   </p>
+  </footer>
+)
